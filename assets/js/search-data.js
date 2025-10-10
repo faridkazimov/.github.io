2bdca42a3f6b004c55813da05ffbd186e1638868
faridkazimov <153233665+faridkazimov@users.noreply.github.com>
2025-10-10 23:06:12 +0000

    Deploying to gh-pages from @ faridkazimov/.github.io@1201d28123a4fddade34b4e4ada66a4faec82bea 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -329,17 +329,6 @@ ninja.data = [{
             window.location.href = "/.github.io/assets/pdf/example_pdf.pdf";
           
         },
-      },{id: "post-a-post-with-diagrams",
-        
-          title: "a post with diagrams",
-        
-        description: "an example of a blog post with diagrams",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/.github.io/blog/2021/diagrams/";
-          
-        },
       },{id: "post-a-distill-style-blog-post",
         
           title: "a distill-style blog post",
